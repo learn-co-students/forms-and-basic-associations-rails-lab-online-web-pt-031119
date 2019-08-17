c1cca708e57701491e03bb7fa5c722ea49303bcd
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-08-17 18:27:12 +0100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   
   belongs_to :artist 
   belongs_to :genre 
+  has_many :notes
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
@@ -10,5 +11,24 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil   
+  end
   
+  def note_contents=(notes)
+    notes.each do |n|
+      self.notes.build(content: n)  unless n.blank?
+    end  
+   
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
 end
